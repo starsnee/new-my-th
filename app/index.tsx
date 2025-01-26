4b1f2d9527b00868818f98dde7f48f453d6f0f0d
Ann-465 <doanminhanh465@gmail.com>
2025-01-25 23:12:24 -0600

    Have functions

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavigationContainer, NavigationIndependentTree } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Game from "./mainpage";
 import FirstPage from './first';
 
 const Stack = createStackNavigator();
+//create the constant data file 
 
+//create changable data file
+// type var = {
+//     wage = 0,
+
+// }
 export default function Index() {
   return (
     <NavigationIndependentTree>
--- app/mainpage.tsx
+++ app/mainpage.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, Button, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import TextBoxWithText from "../assets/text";
+import SwipeableCard from '../assets/swipe'; // Adjust the import path
+import { APR } from '@env'; 
 
-function Game(){
-    <SafeAreaView>
-        <Text>hi there</Text>
+function Game() {
+  return (
+    <SafeAreaView style={styles.container}>
+        <TextBoxWithText/>
+        <SwipeableCard/>
+
+      <Text style={styles.text}>{APR}</Text>
     </SafeAreaView>
-
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
 export default Game;
