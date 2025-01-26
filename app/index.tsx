57bf2b2d11ece9494e920f58d44a1e82bada3fc6
Ann-465 <doanminhanh465@gmail.com>
2025-01-25 20:32:07 -0600

    Debug to switch pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, NavigationIndependentTree } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Game from './mainpage';
+import Game from "./mainpage";
 import FirstPage from './first';
 
 const Stack = createStackNavigator();
 
 export default function Index() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={FirstPage} />
-        <Stack.Screen name="Game" component={Game} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NavigationIndependentTree>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={FirstPage} />
+          <Stack.Screen name="Game" component={Game} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NavigationIndependentTree>
   );
 }
